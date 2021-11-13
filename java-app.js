ff311b33585b73d4c4b2fb83b41ac36234454abe
Bola Oyediran <bola.oyediran@outlook.com>
2021-11-13 04:12:59 +0000

    commit both changes

--- java-app.js
+++ java-app.js
@@ -3,5 +3,9 @@ Welcome from Master
 Welcome from Dev branch
 This application is good
 Devops is good
+<<<<<<< HEAD
 changes made on development
 Final changes on Dev
+=======
+
+>>>>>>> 906cd6b... second release
