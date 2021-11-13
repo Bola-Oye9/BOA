1368e36634a2bb9f756631d7d1b565c2d1c04c7c
Bola Oyediran <bola.oyediran@outlook.com>
2021-11-13 05:27:26 +0000

    resolves 1X

--- java-app.js
+++ java-app.js
@@ -3,14 +3,3 @@ Welcome from Master
 Welcome from Dev branch
 This application is good
 Devops is good
-<<<<<<< HEAD
-<<<<<<< HEAD
-changes made on development
-Final changes on Dev
-=======
-
->>>>>>> 906cd6b... second release
-=======
-changes made on development
-Final changes on Dev
->>>>>>> d14f788... final change
